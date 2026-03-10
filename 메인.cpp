8db5a12e3d94d22224ae43046596683d95afd45b
AN MIN YONG <iks05280204@gmail.com>
2026-03-10 17:21:40 +0900

    템플릿 첫

--- 메인.cpp
+++ 메인.cpp
@@ -2,57 +2,12 @@
 #include <iostream>
 #include "save.h"
 
-class Dog;	// forward declaration
+using namespace std;
 
-void change(int*, int*);
-void change(Dog&, Dog&);
-
-void change(int* a, int* b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-
-
-class Dog {
-private:
-	int num;
-public:
-	Dog() = default;
-	Dog(int n) : num{ n } {}
-
-	operator int() {
-		return num;
-	}
-
-};
-
-void change(Dog& a, Dog& b)
-{
-	Dog temp{ a };
-	a = b;		// operater
-	b = temp;
-}
+// 1학년 << c++의 다형성 ? virtual이 핵심
+// 2학년 << c++에서 template의 역할은 무엇인가? - Generic Programming 구현의 핵심
 
 int main()
 {
-	// [문제] 의도대로 실행되게 하자
-	{
-		// using Dog = int	// #define Dog int 이건 사용하면 안된다.
-		// Dog는 class로 작성하자
-		
-		Dog a{ 1 }, b{ 2 };
-		change(a, b);
-		std::cout << a << ", " << b << std::endl;
-	}
-
-	{
-		int a{ 1 }, b{ 2 };
-		change(&a, &b);
-		std::cout << a << ", " << b << std::endl;
-	}
-
 	//save("메인.cpp");
 }
