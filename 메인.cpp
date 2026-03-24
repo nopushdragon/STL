ef65e8b039716282e853d0cfec83a53765276901
AN MIN YONG <iks05280204@gmail.com>
2026-03-24 17:16:11 +0900

    메모리 만지작

--- 메인.cpp
+++ 메인.cpp
@@ -1,5 +1,12 @@
 //==========================================================================================
-// 많은 수의 자료 다루기 - file i/o
+// 동적 메모리 할당(Dynamic memory allocation)
+// 
+// 정적(static): program 할 때 사용할 메모리를 결정 // compile-time	// 이름을 줄 수 있는 변수 ex) int num; stack, data, code 영역에 존재, code에는 함수
+// 동적(dynamic): 실행한 이후에 결정할 수 있는 메모리 // run-time // 주소로만 이루어진, 이름을 줄 수 없는 변수, free store에 존재
+
+
+// 다음시간 -> 스마트 포인터 -> 콜러블 타입 -> 실습 -> STL
+
 
 #include <iostream>
 #include <fstream>
@@ -9,53 +16,47 @@
 #include <random>
 #include <print>
 #include <string>
+#include <numeric>
+#include <memory>
 #include "save.h"
 
 using namespace std;
 
-default_random_engine dre;
-uniform_int_distribution uid(0, 9999);
-uniform_int_distribution uidNameLen(10, 30);
-uniform_int_distribution<int> uidChar('a', 'z');
-
-class Dog {
-private:
-	string name;	// [10,30] 10이상30이하 길이의 소문자로 구성된 이름  //※(10,30) 10초과30미만
-	size_t id;		// [0,9999]
-
-	friend ostream& operator<<(ostream& os, const Dog& dog) {
-		print(os, "[{:>4}] - {}", dog.id, dog.name);
-		return os;
-	}
-};
-
-// [문제] 파일 "Dog천마리"에는 class Dog 객체 1000개가 저장되어 있다.
-// 파일은 binary mode이고 각 객체는 메모리 크기 그대로 stream의 write 함수로 기록하였다.
-// 모든 객체를 한번의 write 함수로 기록하였다
-// Dog의 멤버는 위와 같다.
-// 메모리로 그대로 읽어 와라.
+// [문제] 사용자가 입력한 수 만큼 int를 저장할 메모리를 확보하라.
+// 1부터 시자가는 정수로 메모리를 채워라.
+// 합계를 화면에 출력하라.
 
 int main()
 {
-	ifstream in("Dog천마리", ios::binary);
-	if (!in) {
-		cout << "Dog 없음" << endl;
-		return 20240601;
+	// int* p; -> 사용하지 말아야함
+	// c++11(modern c++)에 완벽한 대체 수단이 있음 -> smart_pointer
+
+	unique_ptr<int[]> p;	// unique_ptr은 배열도 지원함 -> unique_ptr<int[]> p; -> p[i]로 접근 가능
+	while (1) {
+		size_t input;
+		cout << "몇 개의 메모리를 원하니: ";
+		cin >> input;
+
+		// 설마 음수를 입력하시는 건 아니겠죠? ㅋㅋㅋ << 이거 코파일럿 자동 생성인데 말투 왜이럼
+
+		try {
+			//p = new int[input];
+			p.reset(new int[input]);	// unique_ptr은 reset() 함수를 통해 메모리를 할당할 수 있음
+		}
+		catch (exception& e) {
+			cout << "메모리 고갈됐어욧 - " << e.what() << endl;
+		}
+
+		//iota(p, p + input, 1);
+		//
+		//long long sum = accumulate(p, p + input, 0LL);	//accumulate(p, p + input, static_cast<long long>(0));
+		iota(p.get(), p.get() + input, 1);
+
+		long long sum = accumulate(p.get(), p.get() + input, 0LL);
+		cout << "합계: " << sum << endl;
+
+		//delete[] p; // unique_ptr은 delete를 자동으로 해줌 -> 메모리 누수 방지, 예외 안전성 제공
 	}
-	
-	array<Dog, 1000> dogs;
-	
-	in.read((char*)dogs.data(), dogs.size() * sizeof(Dog));
-	
-	for (const Dog& dog : dogs) {
-		cout << dog << endl;
-	}
-	
-	//array<Dog, 1000> dogs;
-	//for (Dog& dog : dogs) {
-	//	in >> dog;
-	//	cout << dog << endl;
-	//}
 
 	//save("메인.cpp");
 }
