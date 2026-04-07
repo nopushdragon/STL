8418e8b3dc4b6d7ae7a6e501c234878e908277cf
AN MIN YONG <iks05280204@gmail.com>
2026-04-07 17:20:42 +0900

    .

--- 메인.cpp
+++ 메인.cpp
@@ -1,32 +1,49 @@
 //==========================================================================================
-// 중간고사 4월 21일
-
+// STL 컨테이너 - std::String 과 유사한 ZString을 만ㄷ르어서 컨테이너 본질에 접근
 
 #include <iostream>
+#include <array>
+#include <fstream>
 #include "save.h"
 
 using namespace std;
 
+class ZString {
+public:
+	ZString() {
+		cout << "생성() 글자수: " << len << " 객체주소: " << this << " 글자주소: " << (void*)p.get() << endl;
+	};
 
+	ZString(const char* s) {
+		len = strlen(s);
+		p = make_unique<char[]>(len);
+		memcpy(p.get(), s, len);
 
-class Dog {
-private:
-	string name;	// [1,16]
-	int id;			// [0,999'9999]
+		cout << "생성(char*) 글자수: " << len << " 객체주소: " << this << " 글자주소: " << (void*)p.get() << endl;
+	}
 
-	friend ostream& operator<<(ostream& os, const Dog& dog) {
-		print(os,"[{:7}] - {}", dog.id, dog.name);
+	friend ostream& operator<<(ostream& os, const ZString& zs) {
+		for (int i = 0; i < zs.len; ++i) {
+			os << *(zs.p.get() + i);	// os << zs.p[i]; 
+		}
 		return os;
 	}
-};
 
-// [문제] 다운받은 "Dog 십만마리" 에는 class Dog 객체 십만개가 저장되어 있다.
-// 바이너리 모드로 저장하여 정확하게 4MB이다.
-// 메로리로 모두 읽어와라.
-// 앞에서부터 100개 출력하여 확인하라.
+private:
+	size_t len{};
+	unique_ptr<char[]> p{};
+};
 
 int main()
 {
+	// 이 동작 다시 설명하고 ZString으로 바꾸면 안되는 이유부터 시작
+	string s{ "2026년" };
+	string t = move(s);
+
+	cout << "s - " << s << endl;
+	cout << "t - " << t << endl;
+
+
 	//save("메인.cpp");
 
 }
